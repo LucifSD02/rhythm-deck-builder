a0dbfe6ae067891918d73910a80c397406af8f2e
Luci Sheppy <bastiaanvanleusden@gmail.com>
2026-07-20 16:40:56 +0200

    Added an inventory of cards, fixed drag preview

---
+++ scripts/cards/inventory.gd
@@ -0,0 +1,30 @@
+extends GridContainer
+
+const SLOT_SCENE: PackedScene = preload("res://scenes/cards/card_slot.tscn")
+const CARD_SCENE: PackedScene = preload("res://scenes/cards/card.tscn")
+const ROUND = preload("uid://but8r0yq3f17o")
+const PHOTO_2023_03_17_22_45_25 = preload("uid://do6fgi7frdxex")
+const PHOTO_2024_07_23_23_06_26 = preload("uid://b0yot5704cdib")
+const SCREENSHOT_2 = preload("uid://clng4fggbmgfw")
+var images: Array[Texture] = [PHOTO_2023_03_17_22_45_25, PHOTO_2024_07_23_23_06_26, SCREENSHOT_2]
+
+func _ready() -> void:
+	columns = 4
+	generate_grid(12)
+
+func generate_grid(slot_count: int) -> void:
+	for i: int in range(slot_count):
+		var slot_instance: PanelContainer = SLOT_SCENE.instantiate() as PanelContainer
+		add_child(slot_instance)
+		if i < 8:
+			var card_instance: Card = CARD_SCENE.instantiate() as Card
+			slot_instance.add_child(card_instance)
+			var texture_rect: TextureRect = card_instance.get_node("TextureRect") as TextureRect
+			var random_int: int = randi() % 3
+			texture_rect.texture = images[random_int]
+			texture_rect.custom_minimum_size = Vector2(125, 200)
+			var label: Label = card_instance.get_node("Label") as Label
+			label.text = "Item " + str(i + 1)
+			label.mouse_filter = Control.MOUSE_FILTER_IGNORE
+			texture_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+			card_instance.mouse_filter = Control.MOUSE_FILTER_IGNORE
--- scripts/ui/card_slot.gd
+++ scripts/ui/card_slot.gd
@@ -8,15 +8,11 @@ func _ready() -> void:
 func _get_drag_data(_at_position: Vector2) -> Variant:
 	print("drag started")
 	if not current_item:
-		print(current_item)
+		print("no item here")
 		return
 	var data: DragData = DragData.new(self, current_item)
 	print("item node: ", data.item_node, ", origin slot: ", data.origin_slot)
-	var preview: Card = Card.new()
-	preview.texture_rect = TextureRect.new()
-	preview.texture_rect.texture = current_item.texture_rect.texture
-	#preview.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	#preview.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	var preview: Card = current_item.duplicate()
 	preview.custom_maximum_size = current_item.size
 	preview.position = -size / 2
 	set_drag_preview(preview)
@@ -38,12 +34,6 @@ func _drop_data(_at_position: Vector2, data: Variant) -> void:
 		var target_item: Card = current_item
 		remove_child(target_item)
 		origin_slot.add_child(target_item)
-		if origin_slot.has_method("set_current_item"):
-			origin_slot.call("set_current_item", target_item)
-
-	else:
-		if origin_slot.has_method("set_current_item"):
-			origin_slot.call("set_current_item", null)
 
 	dragged_item.get_parent().remove_child(dragged_item)
 	add_child(dragged_item)
@@ -52,11 +42,9 @@ func _drop_data(_at_position: Vector2, data: Variant) -> void:
 func set_current_item(new_item: Card) -> void:
 	current_item = new_item
 
-
 func _on_child_entered_tree(node: Node) -> void:
-	print(node)
+	print("added child ", node)
 	if get_child_count() > 0:
-		print(get_child_count(), "Children")
 		var first_child: Node = get_child(0)
 		if first_child is Card:
 			current_item = first_child
