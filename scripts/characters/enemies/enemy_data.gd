6b5e39ddb0ceb79e940f03faa0c9b80dfbea9aa0
Luci Sheppy <bastiaanvanleusden@gmail.com>
2026-08-15 01:09:47 +0200

    enemi

--- scripts/characters/enemies/enemy_data.gd
+++ scripts/characters/enemies/enemy_data.gd
@@ -5,5 +5,6 @@ const SILENCE = preload("uid://d1ykg17wc4e62")
 const SMALL_BLOCK = preload("uid://buumhuy5ks5uo")
 
 @export var card_inventory: Array[CardBase]
+@export var instrument: BaseInstrument
 @export var name: String
 @export var flavour_text: String
