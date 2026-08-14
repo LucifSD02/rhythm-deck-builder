15f7e6f2f8d8aa895ee2f8829a2559834eb1429c
Luci Sheppy <bastiaanvanleusden@gmail.com>
2026-08-15 00:58:14 +0200

    Started working on an abstract combatant class
    Combatants extend into players or enemies

---
+++ scripts/combat/combatant.gd
@@ -0,0 +1,22 @@
+class_name Combatant
+extends Node
+
+var instrument: BaseInstrument
+var card_inventory: Array[CardBase] = []
+var armour: int
+var stagger_threshold: int
+var status_effects: Array = []  # TODO: needs its own design pass
+var max_energy: int
+var current_energy: int
+
+
+func populate_inventory_from(source: Array[CardBase]) -> void:
+	card_inventory = source.duplicate()
+
+
+func can_afford(card: CardBase) -> bool:
+	return current_energy >= card.energy_cost
+
+
+func spend_energy(card: CardBase) -> void:
+	current_energy -= card.energy_cost
